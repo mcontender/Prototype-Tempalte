d457b2dbb3175ff04ee446d8f2b0d282a35f5dfa
Michael Valeron <mcontender@gmail.com>
2015-05-12 13:24:31 -0400

    Beginning Framework

---
+++ index.php
@@ -0,0 +1,217 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Prototype</title>
+		<link rel="stylesheet" type="text/css" href="/assets/css/global.min.css">
+		<link rel="stylesheet" type="text/css" href="/crossbrand/styleguide/css/fiatusa/global.css">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body id="body-container">
+		
+		<?php include "fiat-header.html";?>
+		<div class="content-block-fill"></div>
+		<?php include "fiat-footer.html";?>
+
+		<section class="wrapper">
+			<div class="row">
+				<div class="col-xl-2">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-2">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-2">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-2">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-2">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-2">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-3">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-3">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-3">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-3">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-4">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-4">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-4">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-5">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-5">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-2">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-6">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-6">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-7">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-5">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-8">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-4">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-9">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-3">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-10">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-2">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-11">
+					<span>TEMP</span>
+				</div>
+				<div class="col-xl-1">
+					<span>TEMP</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-12">
+					<span>TEMP</span>
+				</div>
+			</div>
+		</section>
+		<section class="wrapper">
+			<a id="menu" class="menu-link hide-on-desktop">MENU</a>
+			<div class="row mobile-offscreen">
+				<div class="col-xl-2 col-l-2 col-m-2 col-s-12 col-xs-12">
+					<span>LOGO</span>
+				</div>
+				<div class="col-xl-1 col-l-1 col-m-2 col-s-12 col-xs-12">
+					<span>Vehicles</span>
+					<ul class="">
+						<li>
+							<p>vehicle 1</p>
+						</li>
+						<li>
+							<p>vehicle 2</p>
+						</li>
+						<li>
+							<p>vehicle 3</p>
+						</li>
+						<li>
+							<p>vehicle 4</p>
+						</li>
+						<li>
+							<p>vehicle 5</p>
+						</li>
+						<li>
+							<p>vehicle 6</p>
+						</li>
+					</ul>
+				</div>
+				<div class="col-xl-2 col-l-2 col-m-2 col-s-12 col-xs-12">
+					<span>Shopping Tools</span>
+					<ul class="">
+						<ul>
+							<li>Item 1</li>
+							<li>Item 2</li>
+							<li>Item 3</li>
+						</ul>
+						<ul>
+							<li>Item 1</li>
+							<li>Item 2</li>
+							<li>Item 3</li>
+						</ul>
+						<ul>
+							<li>Item 1</li>
+							<li>Item 2</li>
+							<li>Item 3</li>
+						</ul>
+					</ul>
+				</div>
+				<div class="col-xl-2 col-l-2 col-m-2 col-s-12 col-xs-12">
+					<span>Fiat<sup>&reg;</sup> Nation</span>
+					<ul class="hide-on-mobile">
+						<li>Item 1</li>
+						<li>Item 2</li>
+						<li>Item 3</li>
+						<li>Item 4</li>
+						<li>Item 5</li>
+						<li>Item 6</li>
+						<li>Item 7</li>
+					</ul>
+				</div>
+				<div class="col-xl-1 col-l-1 col-m-2 col-s-12 col-xs-12">
+					<span>Owners</span>
+					<ul class="hide-on-mobile">
+						<li>Item 1</li>
+						<li>Item 2</li>
+						<li>Item 3</li>
+						<li>Item 4</li>
+						<li>Item 5</li>
+						<li>Item 6</li>
+						<li>Item 7</li>
+					</ul>
+				</div>
+				<!-- <div class="col-xl-1 col-l-2 col-m-1">
+						<span>1</span>
+				</div> -->
+				
+				<div class="empty-space col-xl-2 col-l-2 col-m-0">	
+				</div>
+				<div class="col-xl-1 col-l-1 col-m-1 col-s-12 col-xs-12">
+					<span>FAD</span>
+				</div>
+				<div class="col-xl-1 col-l-1 col-m-1 col-s-12 col-xs-12">
+					<span>BMO</span>
+				</div>
+			</div>
+		</section>
+		
+	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="js/main.js"></script>
+</html>
--- js/main.js
+++ js/main.js
@@ -3,13 +3,37 @@
 		--------------------------------------------------- */
 		// private
 		var _self = this;
+		var menuShowing = false;
 		var temp = 'example';
 
 		_self.initialize = function() {
-			console.log('init called');
-			var te = " please ";
+			_self.clickEvents();
 		};
-
+		_self.clickEvents = function() {
+			$('.wrapper .row div span').mouseenter(function(event) {
+				event.preventDefault();
+				$(this).parent().find('ul').addClass('active');
+			});
+			$('.wrapper .row div').mouseleave(function(event) {
+				event.preventDefault();
+				$(this).find('ul').removeClass('active');
+			});
+			$('.wrapper .menu-link').click(function(event) {
+				event.preventDefault();
+				if (menuShowing)
+					$(this).siblings('.row').removeClass('active');
+				// $(this).parents('#body-container').addClass('shift-left');
+				else
+					$(this).siblings('.row').addClass('active');
+				// $(this).parents('#body-container').removeClass('shift-left');
+				menuShowing = !menuShowing;
+			});
+		};
 
 		_self.initialize();
 	}
+
+	var proto;
+	jQuery(document).ready(function() {
+		proto = new Proto(jQuery);
+	});
